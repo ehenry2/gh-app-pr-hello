e3d8fb2e8f6c5db69e25f2efe958a3e5cc637fe1
Eric Henry <eric.henry2172@gmail.com>
2022-12-11 14:24:02 -0500

    config refacotr

--- cmd/gh-app-pr-hello/handler.go
+++ cmd/gh-app-pr-hello/handler.go
@@ -4,43 +4,37 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/ehenry2/gh-app-pr-hello/internal"
-	"github.com/palantir/go-githubapp/githubapp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 )
 
 func main() {
+	// configure logger
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Info().
 		Msg("lambda function starting. parsing config from environment")
+
+	// load configuration
 	ctx := context.Background()
-	appConfig, err := internal.NewConfig(ctx)
+	config, err := internal.NewConfig(ctx)
 	if err != nil {
 		log.Err(err).Msg("failed to read config")
 		os.Exit(1)
 	}
 	log.Info().Msg("parsed config successfully")
+	githubAppConfig := config.ToGithubAppConfig()
 
-	clientConfig := githubapp.Config{
-		V3APIURL: appConfig.GithubV3Endpoint,
-		App: struct {
-			IntegrationID int64  `yaml:"integration_id" json:"integrationId"`
-			WebhookSecret string `yaml:"webhook_secret" json:"webhookSecret"`
-			PrivateKey    string `yaml:"private_key" json:"privateKey"`
-		}{
-			IntegrationID: appConfig.IntegrationID,
-			WebhookSecret: appConfig.WebhookSecret,
-			PrivateKey:    string(appConfig.PrivateKey),
-		},
-	}
+	// register routes
 	log.Info().Msg("registering routes")
-	if err := internal.RegisterGithubWebhookDispatcher(&clientConfig); err != nil {
+	if err := internal.RegisterGithubWebhookDispatcher(githubAppConfig); err != nil {
 		log.Err(err).Msg("failed to load client creator")
 		os.Exit(1)
 	}
 	internal.RegisterHealthCheck()
 	log.Info().Msg("routes registered successfully")
+
+	// start the lambda handler
 	log.Info().Msg("starting lambda handler")
 	lambda.Start(internal.AlbHandler{}.ProxyWithContext)
 }
--- internal/config.go
+++ internal/config.go
@@ -4,15 +4,34 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"github.com/palantir/go-githubapp/githubapp"
 	"github.com/sethvargo/go-envconfig"
 	"io/ioutil"
 )
 
+type GithubAuthConfig struct {
+	IntegrationID int64  `yaml:"integration_id" json:"integrationId"`
+	WebhookSecret string `yaml:"webhook_secret" json:"webhookSecret"`
+	PrivateKey    string `yaml:"private_key" json:"privateKey"`
+}
+
 type Config struct {
 	IntegrationID    int64  `env:"GITHUB_INTEGRATION_ID,required"`
 	WebhookSecret    string `env:"GITHUB_WEBHOOK_SECRET,required"`
-	PrivateKey       []byte `env:"GITHUB_PRIVATE_KEY,required"`
+	PrivateKeyBytes  []byte `env:"GITHUB_PRIVATE_KEY,required"`
 	GithubV3Endpoint string `env:"GITHUB_V3_ENDPOINT,required"`
+	PrivateKey       string
+}
+
+func (c *Config) ToGithubAppConfig() *githubapp.Config {
+	return &githubapp.Config{
+		V3APIURL: c.GithubV3Endpoint,
+		App: GithubAuthConfig{
+			IntegrationID: c.IntegrationID,
+			WebhookSecret: c.WebhookSecret,
+			PrivateKey:    c.PrivateKey,
+		},
+	}
 }
 
 func NewConfig(ctx context.Context) (*Config, error) {
@@ -20,11 +39,11 @@ func NewConfig(ctx context.Context) (*Config, error) {
 	if err := envconfig.Process(ctx, &config); err != nil {
 		return &config, err
 	}
-	dec := base64.NewDecoder(base64.StdEncoding, bytes.NewBuffer(config.PrivateKey))
+	dec := base64.NewDecoder(base64.StdEncoding, bytes.NewBuffer(config.PrivateKeyBytes))
 	b, err := ioutil.ReadAll(dec)
 	if err != nil {
 		return &config, err
 	}
-	config.PrivateKey = b
+	config.PrivateKey = string(b)
 	return &config, err
 }
